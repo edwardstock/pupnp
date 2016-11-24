0315f311622af7c6740060b508a38a1e383c5733
Eduard Maximovich <edward.vstock@gmail.com>
2016-11-24 21:08:19 +0300

    autodeploy to release new builds

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -1,13 +1,41 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-DIST="libupnp_$(uname -p)_$(git describe --tags).tar.tz"
+BUILD_TYPE=$1
+METHOD=$2
+OS=$(uname -s)
+ARCH=$(uname -m)
+VERSION=$(git describe --tags)
+ARCHIVER="tar -zcvf"
 
-ARG=$1
-
-if [ $ARG == "getname" ]
+if [ $BUILD_TYPE != "shared" ] && [ $BUILD_TYPE != "static"  ]
 then
-    echo $DIST
-    exit
+	echo "only shared and static builds supported, given: ${BUILD_TYPE}"
+	exit 1
 fi
 
-tar -zcvf $NAME
+if [ -z "${CC}" ]
+then
+	CC=gcc # default compiler is gcc
+fi
+
+DIST="libupnp_${ARCH}_${VERSION}_${CC}_${BUILD_TYPE}.tar.gz"
+
+if [ "${METHOD}" == "getname" ]
+then
+    echo $DIST
+    exit 0
+fi
+
+mkdir dist
+
+ARCHIVER="${ARCHIVER} dist/${DIST}"
+
+if [ $BUILD_TYPE == "shared" ]
+then
+	FILES=$(find ./prebuild/${OS} -name "*.dll" -o -name "*.dll.a" -o -name "*.so" -o -name "*.dylib")
+else
+	FILES=$(rm -f ./prebuild/${OS}/*.dll.a && find ./prebuild/${OS} -name "*.a")
+fi
+
+$(${ARCHIVER} ${FILES})
+
